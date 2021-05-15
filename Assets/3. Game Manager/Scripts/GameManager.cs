bcfc53bc981d9bfc6156cfd893e4a0c4b4ff0e82
hiroto302 <i.am.a.hiroto@gmail.com>
2021-05-15 18:24:07 +0900

    Writing the GameManager - GameManagers as Singletons

--- Assets/3. Game Manager/Scripts/GameManager.cs
+++ Assets/3. Game Manager/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     // what level the game is current in
     // load and Unload game levels
